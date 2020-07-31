1ca5a7cbb63cca676a965b8ef63da91a346548f9
Jeffrey Barton <jeffrey.barton99@users.noreply.github.com>
2020-07-30 21:48:30 -0500

    renamed folders to turn in homework

--- JBTaskList/Program.cs
+++ JBTaskList/Program.cs
@@ -4,37 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JBTaskList
+namespace TaskList
 {
     class Program
     {
-        /* https://www.c-sharpcorner.com/article/c-sharp-list/, and 
-           https://forgetcode.com/csharp/2158-create-a-list-dynamically-from-user-input used
-         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Insert task");
-            String input = Console.ReadLine();
-            List<string> TaskList = new List<string>();
-            TaskList.Add(input);
-            String inputNewTask = Console.ReadLine();
-            TaskList.Add(inputNewTask);
-            foreach (var task in TaskList) 
-            {
-                Console.WriteLine(task);
-            }
+            new TaskApp().Run();
         }
-     
-       
-
-        
-            
-    } 
-}       
-
-        
-           /* {
-                List<string> MyTasks = new List<string>();
-                string[] tasks = { "laundry", "garbage disposal", "homework", "store" };
-                List<string> tasksList = new List<string>(tasks);
-            } */
+    }
+}
